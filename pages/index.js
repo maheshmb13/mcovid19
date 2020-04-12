7ecdd0479c771aed91bb9ec212d0ddada2e75d63
maheshmb13 <maheshbabu.p2001@gmail.com>
2020-04-12 15:59:57 +0530

    next

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,47 @@
 import Head from 'next/head'
-
+import Link from 'next/link'
+import OfflineBoltIcon from '@material-ui/icons/OfflineBolt';
+import OpacityIcon from '@material-ui/icons/Opacity';
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>MB | Covid19</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome <OfflineBoltIcon />
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        Covid-19 statistics
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <Link href="/world">
+        <a className="card">
+          <h3>World Wide &rarr;</h3>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
+          </Link>
+        <Link href="/india">
+        <a className="card">
+          <h3>India &rarr;</h3>
+          <p></p>
+        </a>
+        </Link>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
       </div>
     </main>
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Powered by MB <OpacityIcon/>
       </a>
     </footer>
 
